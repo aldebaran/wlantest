d3cbaa80d8b0f87afffc6925a2ca9aafd753438c
Maxence Viallon <mviallon@aldebaran-robotics.com>
2012-06-29 18:32:02 +0200

    script : killing hostap after testing

--- scripts/wifi.py
+++ scripts/wifi.py
@@ -7,22 +7,30 @@
 WORK_DIR = "/home/maxence/src/wlantest"
 
 import subprocess
-import os
+import os,signal
 from time import sleep
 
-def launch_hostap(conf):
+def mode_process(conf):
+    # On lance hostap
     cmd = ["hostapd", WORK_DIR+"/hostap/"+conf, "-d"]
     proc = subprocess.Popen(cmd)
     sleep(3)
+
+    # TODO : Se connecter à l'AP avec connman
+
+    # On tue hostap
+    os.kill(proc.pid, signal.SIGTERM)
 
 if (__name__ == "__main__"):
     # On liste les differentes conf hostap
     print ("Reading hostap directory ...")
     MODES = os.listdir(WORK_DIR+"/hostap/") 
     print MODES
+    
+    # TODO : Lancer le dhcp
 
     # Boucle principale
     for mode in MODES:
-        launch_hostap(mode)
+        mode_process(mode)
         print("Hostap running mode "+mode+"!")
 
