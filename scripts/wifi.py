d3af2e4b46bb9edd97f2a36268fb8213467c27d3
Maxence Viallon <mviallon@aldebaran-robotics.com>
2012-07-03 16:30:54 +0200

    script : wifi script modified to take advantage of classes

--- scripts/wifi.py
+++ scripts/wifi.py
@@ -5,56 +5,45 @@
 #
 
 WORK_DIR = "/home/maxence/src/wlantest"
-IP_AP = "192.168.3.1"
 
-import subprocess
-import os,signal
+import os
 from time import sleep
 
-def scan_wifi():
-    cmd = ["/home/maxence/src/connman/test/test-connman", "scan", "wifi"]
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-
-def ping_ap():
-    cmd = ["ping", "-c", "1", IP_AP]
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-    return proc.returncode
+from ConnmanClient import ConnmanClient
+from Hostapd import Hostapd
 
 def mode_process(conf):
     # On lance hostap
-    cmd = ["hostapd", WORK_DIR+"/hostap/"+conf, "-d"]
-    proc = subprocess.Popen(cmd)
-    sleep(3)
+    hostapd.reload(conf)
     print("Hostap running mode "+mode+"!")
     
     # On scan les réseaux
     print("Scanning wifi ...")
-    scan_wifi()
-
+    connman.scan()
+    
     # On laisse à connman le temps de se connecter
-    sleep(3)
-
+    sleep(5)
+    
     # On teste la connectivité
-    if not ping_ap():
-        print ("Sucess !")
-    else:
-        print ("Failure !")
-
-    # On tue hostap
-    os.kill(proc.pid, signal.SIGTERM)
-    sleep(1)
-
+    connman.serviceisConnected()
+    
 if (__name__ == "__main__"):
+    
     # On liste les differentes conf hostap
     print ("Reading hostap directory ...")
     MODES = os.listdir(WORK_DIR+"/hostap/") 
     print MODES
     
     # TODO : Lancer le dhcp
-
+    
+    # Création des objets
+    connman = ConnmanClient()
+    hostapd = Hostapd()
+    
     # Boucle principale
     for mode in MODES:
         mode_process(mode)
+
+    # Killing Hostapd
+    hostapd.kill()
 
