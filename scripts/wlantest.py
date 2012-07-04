5696cd353eb120e3d1bef641c01c4febf9cb6134
Maxence Viallon <mviallon@aldebaran-robotics.com>
2012-07-04 18:13:01 +0200

    script : add AgentThread class

--- scripts/wlantest.py
+++ scripts/wlantest.py
@@ -9,17 +9,32 @@
 ##
 
 import os
+import threading
 from time import sleep
 
 from ConnmanClient import ConnmanClient
 from Hostapd import Hostapd
+
+class AgentThread (threading.Thread):
+
+    def __init__(self, daemon):
+        threading.Thread.__init__(self)
+        self.daemon = daemon
+
+
+    def run(self):
+        # TODO : Agent main loop
+        pass
 
 class wlantest:
 
     def __init__(self):
         self.connman = ConnmanClient()
         self.hostapd = Hostapd()
+        self.agent = AgentThread(True)
 
+        self.agent.start()
+    
     def wpa2(self):
         self.hostapd.reload("wpa2.conf")
         print("Hostap running mode wpa2")
@@ -44,7 +59,7 @@ if (__name__ == "__main__"):
     # TODO : Start dhcp
     
     wlantest = wlantest()
-    
+
     wlantest.wpa2()
 
     wlantest.stop()
