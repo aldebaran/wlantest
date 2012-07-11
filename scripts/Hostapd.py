ad75f4983cb2bfd3f7128b6006a1c5620052b5a4
Maxence Viallon <mviallon@aldebaran-robotics.com>
2012-07-11 15:52:26 +0200

    script : add Config class to write conf file

--- scripts/Hostapd.py
+++ scripts/Hostapd.py
@@ -7,19 +7,31 @@
 ##
 
 #HOSTAP_DIR = "/usr/local/bin"
-#CONF_DIR = "/usr/share/wlantest/hostap"
-CONF_DIR = "/home/maxence/src/wlantest/hostap"
-#TEMP_FILE = "/home/nao/hostapd.conf"
-TEMP_FILE = "/home/maxence/src/hostap/hostapd/hostapd.conf"
+CONF_FILE = "/home/maxence/src/hostap/hostapd/hostapd.conf"
+IFACE = "wlan1"
 
 import subprocess
-import shutil
 import os,signal
 from time import sleep
 
+class Config:
+    """
+    Class to write conf file
+    """
+
+    def __init__(self):
+        self.config = open ("hostapd.conf", "w")
+        self.set("interface", IFACE)
+
+    def set(self, key, value):
+        self.config.write("%s = %s\n" %(key,value))
+
+    def push(self):
+        self.config.close()
+
 class Hostapd:
     """
-    Class to configure hostapd
+    Class to manage hostapd
     """
 
     def __init__(self):
@@ -27,14 +39,29 @@ class Hostapd:
         self.proc = subprocess.Popen(self.cmd)
         sleep(3)
 
-    def reload(self, conf):
-        # Copying new conf
-        shutil.copy(CONF_DIR+"/"+conf, TEMP_FILE)
+    def wpa2(self, ssid = "wpa2rezo", passphrase="12345678"):
 
-        # Reloading hostap
+        config = Config()
+
+        config.set("ssid", ssid)
+        config.set("wpa_passphrase", passphrase)
+
+        config.push()
+
+        self.reload()        
+
+    def wpa(self, ssid = "wparezo", passphrase="12345678"):
+        pass
+
+    def reload(self):
         os.kill(self.proc.pid, signal.SIGHUP)
         sleep(1)
-    
+
     def kill(self):
         os.kill(self.proc.pid, signal.SIGTERM)
         sleep(1)
+
+if (__name__ == "__main__"):
+
+    myhost = Hostapd()
+    myhost.wpa2("wpatest","123")
