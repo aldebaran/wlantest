f7c34f0269e5f4a805c07f973d81e733d69506b2
Maxence Viallon <mviallon@aldebaran-robotics.com>
2012-07-12 10:17:17 +0200

    script : no need for ssid and passphrase to be stored locally

--- scripts/Hostapd.py
+++ scripts/Hostapd.py
@@ -60,10 +60,8 @@ class Hostapd:
         config = Config()
 
         config.set("ssid", ssid)
-        self.ssid = ssid
         config.set("wpa", "2")
         config.set("wpa_passphrase", passphrase)
-        self.passphrase = passphrase
         config.set("wpa_key_mgmt", "WPA-PSK")
         config.set("wpa_pairwise", "CCMP")
 
@@ -75,10 +73,8 @@ class Hostapd:
         config = Config()
 
         config.set("ssid", ssid)
-        self.ssid = ssid
         config.set("wpa", "1")
         config.set("wpa_passphrase", passphrase)
-        self.passphrase = passphrase
         config.set("wpa_key_mgmt", "WPA-PSK")
         config.set("wpa_pairwise", "TKIP")
 
