263a07a9fcb01a6366c337478be41f36047efe38
Maxence Viallon <maxence.viallon@orange.fr>
2012-08-27 14:23:43 +0200

    __init__: add brief description of package .py files

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+    *wlantest.py
+Main class, loads and runs Wlantest tests.
+
+    *connmanclient.py
+Interacts with ConnMan and controls a ConnMan agent.
+
+    *hostapd.py
+Generates hostapd configuration and manages hostapd subprocess to create a wifi AP.
+
+"""
